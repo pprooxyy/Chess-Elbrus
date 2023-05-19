9a7706b1e416e4697954b64fe3d68e913333d7d0
pprooxyy <scvortsovmaxim@gmail.com>
2023-05-19 12:27:31 +0300

    init-bug-fix

--- client/src/components/molecules/MainPagePlaySection/MainpagePlaySection.tsx
+++ client/src/components/molecules/MainPagePlaySection/MainpagePlaySection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../../atoms/Button/Button";
-import "./MainPlaySection.css";
+import "./MainPagePlaySection.css";
 
 function MainPlaySection() {
   const boardStyles = {
