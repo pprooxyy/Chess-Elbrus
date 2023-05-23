fd3b897225de3cc05a6dc83779e66cae581845a6
NikitaShaidorov <nshaydorov@gmail.com>
2023-05-23 12:21:48 +0300

    seach friends

--- client/src/components/molecules/Friends/Friends.tsx
+++ client/src/components/molecules/Friends/Friends.tsx
@@ -11,6 +11,7 @@ interface Friend {
 
 const Friends: React.FC = () => {
   const [friends, setFriends] = useState<Friend[]>([]);
+  const [users, setUsers] = useState<Friend[]>([]);
   const [newFriendName, setNewFriendName] = useState<string>('');
   const [errorMsg, setErrorMsg] = useState<string>("");
 
@@ -31,6 +32,7 @@ const Friends: React.FC = () => {
       if (response.ok) {
         const data = await response.json();
         setFriends(data);
+        setUsers([]);
       } else {
         console.error('Failed to fetch friends:', response.statusText);
       }
@@ -39,26 +41,58 @@ const Friends: React.FC = () => {
     }
   };
 
-  const addFriend = async (): Promise<void> => {
+  const searchUsers = async (name: string): Promise<void> => {
     try {
+      if (name.length === 0) return fetchFriends()
       const response = await fetch('http://localhost:3001/friends', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name: newFriendName }),
+        body: JSON.stringify({ name: name }),
         credentials: "include"
       });
 
       if (response.ok) {
-        setNewFriendName('');
-        fetchFriends();
+        const data = await response.json();
+        console.log(data);
+        setFriends(data.friends);
+        setUsers(data.users);
       } else if (response.status === 404) {
-       setErrorMsg('Friend not found. Please check the username.');
-       
-       setTimeout(() => {
-        setErrorMsg('');
-      }, 3000);
+        setErrorMsg('Friend not found. Please check the username.');
+        setFriends([]);
+        setTimeout(() => {
+          setErrorMsg('');
+        }, 3000);
+      }
+      else {
+        console.error('Failed to add friend:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Failed to add friend:', error);
+    }
+  };
+
+  const addFriend = async (friendId: number): Promise<void> => {
+    try {
+      const response = await fetch(`http://localhost:3001/friends/${friendId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: "include"
+      });
+
+      if (response.ok) {
+        searchUsers(newFriendName);
+        // setNewFriendName('');
+        // fetchFriends();
+      } else if (response.status === 404) {
+        setErrorMsg('Friend not found. Please check the username.');
+
+        setTimeout(() => {
+          setErrorMsg('');
+        }, 3000);
       }
       else {
         console.error('Failed to add friend:', response.statusText);
@@ -79,7 +113,7 @@ const Friends: React.FC = () => {
       });
 
       if (response.ok) {
-        fetchFriends();
+        searchUsers(newFriendName);
       } else {
         console.error('Failed to delete friend:', response.statusText);
       }
@@ -89,8 +123,8 @@ const Friends: React.FC = () => {
   };
 
   const handleButtonClick = () => {
-    addFriend();
-    setNewFriendName('');
+    searchUsers(newFriendName);
+    // setNewFriendName('');
   };
 
   return (
@@ -102,16 +136,19 @@ const Friends: React.FC = () => {
           <input
             type="text"
             value={newFriendName}
-            onChange={(e) => setNewFriendName(e.target.value)}
+            onChange={(e) => {
+              setNewFriendName(e.target.value);
+              searchUsers(e.target.value);
+            }}
             placeholder="Enter your friend name"
             className="friend-input"
           />
-          <button onClick={handleButtonClick} className="add-button">Add</button>
+          <button onClick={handleButtonClick} className="add-button">Search</button>
         </div>
       </div>
-        {errorMsg && <p className="error-message">{errorMsg}</p>}
+      {errorMsg && <p className="error-message">{errorMsg}</p>}
       {friends.length === 0 ? (
-        <p className='no-friend-list'>Friends list is empty, add a friend by nickname</p>
+        <p className='no-friend-list'>No friends found</p>
       ) : (
         <div className="friend-cards">
           {friends.map((friend) => (
@@ -119,12 +156,23 @@ const Friends: React.FC = () => {
               <a href={`/profile/${friend.id}`} className="friend-name"><Card.Img variant="top" src={friend.avatar} alt={friend.name} className='avatar' /></a>
               <Card.Body>
                 <a href={`/profile/${friend.id}`} className="friend-name"><Card.Title>{friend.name}</Card.Title></a>
-                <Button variant="danger" onClick={() => deleteFriend(friend.id)} className="custom-button">Delete</Button>
+                <Button variant="danger" onClick={() => deleteFriend(friend.id)} className="custom-button">DELETE</Button>
               </Card.Body>
             </Card>
           ))}
         </div>
       )}
+      {<div className="friend-cards">
+        {users.map((user) => (
+          <Card key={user.id} className="friend-card">
+            <a href={`/profile/${user.id}`} className="friend-name"><Card.Img variant="top" src={user.avatar} alt={user.name} className='avatar' /></a>
+            <Card.Body>
+              <a href={`/profile/${user.id}`} className="friend-name"><Card.Title>{user.name}</Card.Title></a>
+              <Button variant="danger" onClick={() => addFriend(user.id)} className="custom-button">ADD</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>}
     </div>
   );
 };
