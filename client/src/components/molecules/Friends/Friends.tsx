2012b057f6d65ce0911e2c734cb3f1bac6c316a3
Anton Dryuchin <antondryuchin@gmail.com>
2023-05-25 17:37:02 +0400

    styles final

--- client/src/components/molecules/Friends/Friends.tsx
+++ client/src/components/molecules/Friends/Friends.tsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Friends.css";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 interface Friend {
   id: number;
   name: string;
   avatar: string;
-  rating: number
+  rating: number;
 }
 
 const Friends: React.FC = () => {
   const [friends, setFriends] = useState<Friend[]>([]);
   const [users, setUsers] = useState<Friend[]>([]);
-  const [newFriendName, setNewFriendName] = useState<string>('');
+  const [newFriendName, setNewFriendName] = useState<string>("");
   const [errorMsg, setErrorMsg] = useState<string>("");
 
   useEffect(() => {
@@ -23,12 +23,12 @@ const Friends: React.FC = () => {
 
   const fetchFriends = async (): Promise<void> => {
     try {
-      const response = await fetch('http://localhost:3001/friends', {
-        method: 'GET',
+      const response = await fetch("http://localhost:3001/friends", {
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        credentials: "include"
+        credentials: "include",
       });
 
       if (response.ok) {
@@ -36,23 +36,23 @@ const Friends: React.FC = () => {
         setFriends(data);
         setUsers([]);
       } else {
-        console.error('Failed to fetch friends:', response.statusText);
+        console.error("Failed to fetch friends:", response.statusText);
       }
     } catch (error) {
-      console.error('Failed to fetch friends:', error);
+      console.error("Failed to fetch friends:", error);
     }
   };
 
   const searchUsers = async (name: string): Promise<void> => {
     try {
-      if (name.length === 0) return fetchFriends()
-      const response = await fetch('http://localhost:3001/friends', {
-        method: 'POST',
+      if (name.length === 0) return fetchFriends();
+      const response = await fetch("http://localhost:3001/friends", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ name: name }),
-        credentials: "include"
+        credentials: "include",
       });
 
       if (response.ok) {
@@ -61,66 +61,70 @@ const Friends: React.FC = () => {
         setFriends(data.friends);
         setUsers(data.users);
       } else if (response.status === 404) {
-        setErrorMsg('Friend not found. Please check the username.');
+        setErrorMsg("Friend not found. Please check the username.");
         setFriends([]);
         setTimeout(() => {
-          setErrorMsg('');
+          setErrorMsg("");
         }, 3000);
-      }
-      else {
-        console.error('Failed to add friend:', response.statusText);
+      } else {
+        console.error("Failed to add friend:", response.statusText);
       }
     } catch (error) {
-      console.error('Failed to add friend:', error);
+      console.error("Failed to add friend:", error);
     }
   };
 
   const addFriend = async (friendId: number): Promise<void> => {
     try {
-      const response = await fetch(`http://localhost:3001/friends/${friendId}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: "include"
-      });
+      const response = await fetch(
+        `http://localhost:3001/friends/${friendId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
 
       if (response.ok) {
         searchUsers(newFriendName);
         // setNewFriendName('');
         // fetchFriends();
       } else if (response.status === 404) {
-        setErrorMsg('Friend not found. Please check the username.');
+        setErrorMsg("Friend not found. Please check the username.");
 
         setTimeout(() => {
-          setErrorMsg('');
+          setErrorMsg("");
         }, 3000);
-      }
-      else {
-        console.error('Failed to add friend:', response.statusText);
+      } else {
+        console.error("Failed to add friend:", response.statusText);
       }
     } catch (error) {
-      console.error('Failed to add friend:', error);
+      console.error("Failed to add friend:", error);
     }
   };
 
   const deleteFriend = async (friendId: number): Promise<void> => {
     try {
-      const response = await fetch(`http://localhost:3001/friends/${friendId}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: "include"
-      });
+      const response = await fetch(
+        `http://localhost:3001/friends/${friendId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
 
       if (response.ok) {
         searchUsers(newFriendName);
       } else {
-        console.error('Failed to delete friend:', response.statusText);
+        console.error("Failed to delete friend:", response.statusText);
       }
     } catch (error) {
-      console.error('Failed to delete friend:', error);
+      console.error("Failed to delete friend:", error);
     }
   };
 
@@ -131,7 +135,7 @@ const Friends: React.FC = () => {
 
   return (
     <div className="friend-container">
-      <h2 className='title-friend'>Friends</h2>
+      <h2 className="title-friend">Friends</h2>
       <div className="puzzle-content-wrapper">
         <div className="add-friend">
           <h3>Add friends</h3>
@@ -142,53 +146,91 @@ const Friends: React.FC = () => {
               setNewFriendName(e.target.value);
               searchUsers(e.target.value);
             }}
-            placeholder="Enter your friend name"
+            placeholder="Enter name"
             className="friend-input"
           />
-          <button onClick={handleButtonClick} className="add-button">Search</button>
+          <button onClick={handleButtonClick} className="add-button">
+            Search
+          </button>
         </div>
       </div>
       {errorMsg && <p className="error-message">{errorMsg}</p>}
       {friends.length === 0 ? (
         <>
-          <p className='no-friend-list'>Friends no added</p>
+          <p className="no-friend-list">Friends no added</p>
           <div className="line-up"></div>
         </>
-
       ) : (
         <div className="friend-cards">
           {friends.map((friend) => (
             <div className="inside-card">
               <Card key={friend.id} className="friend-card">
                 <div className="avatar-name">
-                  <Link to={`/profile/${friend.id}`} className="friend-name"><Card.Img variant="top" src={friend.avatar} alt={friend.name} className='avatar' /></Link>
+                  <Link to={`/profile/${friend.id}`} className="friend-name">
+                    <Card.Img
+                      variant="top"
+                      src={friend.avatar}
+                      alt={friend.name}
+                      className="avatar"
+                    />
+                  </Link>
                   <Card.Body>
-                    <Link to={`/profile/${friend.id}`} className="friend-name"><Card.Title>{friend.name}<br />{friend.rating}</Card.Title></Link>
+                    <Link to={`/profile/${friend.id}`} className="friend-name">
+                      <Card.Title>
+                        {friend.name}
+                        <br />
+                        {friend.rating}
+                      </Card.Title>
+                    </Link>
                   </Card.Body>
                 </div>
-                <Button variant="danger" onClick={() => deleteFriend(friend.id)} className="custom-button1">DELETE</Button>
+                <Button
+                  variant="danger"
+                  onClick={() => deleteFriend(friend.id)}
+                  className="custom-button1"
+                >
+                  DELETE
+                </Button>
               </Card>
             </div>
           ))}
         </div>
-      )
-      }
+      )}
       {
         <div className="friend-cards">
           {users.map((user) => (
             <div className="inside-card">
               <Card key={user.id} className="friend-card">
-                <Link to={`/profile/${user.id}`} className="friend-name"><Card.Img variant="top" src={user.avatar} alt={user.name} className='avatar' /></Link>
+                <Link to={`/profile/${user.id}`} className="friend-name">
+                  <Card.Img
+                    variant="top"
+                    src={user.avatar}
+                    alt={user.name}
+                    className="avatar"
+                  />
+                </Link>
                 <Card.Body>
-                  <Link to={`/profile/${user.id}`} className="friend-name"><Card.Title>{user.name}<br />{user.rating}</Card.Title></Link>
+                  <Link to={`/profile/${user.id}`} className="friend-name">
+                    <Card.Title>
+                      {user.name}
+                      <br />
+                      {user.rating}
+                    </Card.Title>
+                  </Link>
                 </Card.Body>
-                <Button variant="danger" onClick={() => addFriend(user.id)} className="custom-button2">ADD</Button>
+                <Button
+                  variant="danger"
+                  onClick={() => addFriend(user.id)}
+                  className="custom-button2"
+                >
+                  ADD
+                </Button>
               </Card>
             </div>
           ))}
         </div>
       }
-    </div >
+    </div>
   );
 };
 
--- client/src/components/organisms/History/History.tsx
+++ client/src/components/organisms/History/History.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../redux/typesRedux";
 import "./History.css";
-import { getHistory } from "../../../redux/thunk/history/history"
+import { getHistory } from "../../../redux/thunk/history/history";
 import { Link } from "react-router-dom";
 import Chessboard from "chessboardjsx";
 import { Chess } from "chess.js";
 export default function History() {
-
   const dispatch = useAppDispatch();
   //
   useEffect(() => {
     dispatch(getHistory());
-  }, [])
-  const history = useAppSelector(state => state.historySlicer.history);
-  console.log(history, 'in the history component');
+  }, []);
+  const history = useAppSelector((state) => state.historySlicer.history);
+  console.log(history, "in the history component");
 
   return (
     <>
@@ -21,17 +20,25 @@ export default function History() {
       {history.map((game) => (
         <div className="history-wrapper" key={game.id}>
           <div className="names-and-date">
-            <h3>
-              <Link to={`/profile/${game.player1Id}`}>{game.player1}</Link>
-              {' VS '}
-              <Link to={`/profile/${game.player2Id}`}>{game.player2}</Link>
-            </h3>
-            <h3> {game.game_start_time} </h3>
+            <p>
+              <b>
+                <Link to={`/profile/${game.player1Id}`}>{game.player1}</Link>
+              </b>
+            </p>
+            {" VS "}
+            <p>
+              <b>
+                <Link to={`/profile/${game.player2Id}`}>{game.player2}</Link>
+              </b>
+            </p>
+            <br />
+            <h3>Game started:</h3>
+            <h3>{game.game_start_time} </h3>
           </div>
           <div className="board-container">
             <Chessboard
               position={game.game_fen}
-              width={300}
+              width={200}
               darkSquareStyle={{ backgroundColor: "#B7C0D8" }}
               lightSquareStyle={{ backgroundColor: "#E8EDF9" }}
               draggable={false}
@@ -43,14 +50,19 @@ export default function History() {
                 <img className="board-png" src="./assets/board.png" alt="board" />
               </div> */}
           <div className="game-status-and-duration">
-            <h3>{game.tie ? ("IT'S A TIE!!!") : (<Link to={`/profile/${game.winnerId}`}>{`WINNER: ${game.winner}`}</Link>)}</h3>
-            <h3>{`DURATION: ${game.duration}`}</h3>
+            <h3>
+              {game.tie ? (
+                "IT'S A TIE!"
+              ) : (
+                <Link
+                  to={`/profile/${game.winnerId}`}
+                >{`Winner: ${game.winner}`}</Link>
+              )}
+            </h3>
+            <h3>{`Duration: ${game.duration}`}</h3>
           </div>
         </div>
       ))}
     </>
-
-
-
   );
 }
